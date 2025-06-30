59ee7cfb57569231d5ec12e2a9cb9f7c3ca8718d
林立洋 <lilyang@example.com>
2025-06-30 14:27:19 +0800

    change configversion = >= 5.95, < 6.0.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.95, < 6.0.0"
     }
   }
 
@@ -13,6 +13,7 @@ terraform {
     encrypt        = true
   }
 }
+
 
 provider "aws" {
   region = "ap-northeast-1"
