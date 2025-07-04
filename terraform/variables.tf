cae39752f01d2a701cdaa283f667bb4b50664146
林立洋 <lilyang@example.com>
2025-07-04 17:41:32 +0800

    edit alot of things

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,11 +4,6 @@ variable "region" {
   default     = "ap-northeast-1"
 }
 
-variable "subnet_ids" {
-  description = "List of subnet IDs where EKS cluster control plane (ENIs) will be provisioned"
-  type        = list(string)
-}
-
 variable "iam_user" {
   description = "I AM user name to be granted admin access to the EKS cluster"
   type        = string
