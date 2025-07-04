0c05e18bb862ef84ae3f93f5cc988e22b4d69c3e
林立洋 <lilyang@example.com>
2025-07-04 15:47:26 +0800

    edit alot of things

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,22 +43,6 @@ module "eks" {
   }
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-  depends_on = [module.eks]  # 確保先建立 Cluster
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-  depends_on = [module.eks]
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
 resource "kubernetes_deployment" "my_app" {
   depends_on = [module.eks]
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,13 +4,6 @@ variable "region" {
   default     = "ap-northeast-1"
 }
 
-# 如果你還有其他變數，也可以像下面這樣定義：
-# variable "db_password" {
-#   description = "Password for the RDS instance"
-#   type        = string
-#   sensitive   = true
-# }
-
 variable "subnet_ids" {
   description = "List of subnet IDs where EKS cluster control plane (ENIs) will be provisioned"
   type        = list(string)
