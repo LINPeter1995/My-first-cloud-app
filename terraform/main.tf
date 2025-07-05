b4cc689e51cc56a88e7c63ec61cd9a534dee2be9
林立洋 <lilyang@example.com>
2025-07-05 21:00:40 +0800

    edit alot of things

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,9 +74,7 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  config_path = "~/.kube/config"
 }
 
 resource "aws_ecr_repository" "my_app_repo" {
